562976efb4cb02a7f8b4b47d716cdd7770cdef52
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-06-21 16:39:09 +0530

    changes

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -5,7 +5,7 @@ function Loader() {
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center bg-[#cb9191] text-white">
       <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-[#FF4D4D] mb-4"></div>
-      <p className='text-lg'>Loading Portfolio...</p>
+      <p className='text-4xl font-extrabold '>Welcome to My Portfolio...</p>
     </div>
   );
 }
